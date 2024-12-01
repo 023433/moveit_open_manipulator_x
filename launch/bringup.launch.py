8b83e3626fb16a36a2da6d01d3f0e4f380217311
023433 <023433@gmail.com>
2024-12-01 13:04:15 +0900

    send

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -51,34 +51,6 @@ def generate_launch_description():
   ld.add_action(
     DeclareBooleanLaunchArg("publish_monitored_planning_scene", default_value=True)
   )
-  # load non-default MoveGroup capabilities (space separated)
-  ld.add_action(
-    DeclareLaunchArgument(
-      "capabilities",
-      default_value=moveit_config.move_group_capabilities["capabilities"],
-    )
-  )
-  # inhibit these default MoveGroup capabilities (space separated)
-  ld.add_action(
-    DeclareLaunchArgument(
-      "disable_capabilities",
-      default_value=moveit_config.move_group_capabilities["disable_capabilities"],
-    )
-  )
-
-  # do not copy dynamics information from /joint_states to internal robot monitoring
-  # default to false, because almost nothing in move_group relies on this information
-  ld.add_action(DeclareBooleanLaunchArg("monitor_dynamics", default_value=False))
-
-  ld.add_action(
-    Node(
-      package='open_manipulator_x_controller',
-      executable='open_manipulator_x_controller',
-      name='open_manipulator_x_controller',
-      arguments=[usb_port, baud_rate],
-      parameters=[param_dir],
-      output='screen')
-  )
 
   ld.add_action(
     Node(
@@ -141,14 +113,6 @@ def generate_launch_description():
   move_group_configuration = {
     "publish_robot_description_semantic": True,
     "allow_trajectory_execution": LaunchConfiguration("allow_trajectory_execution"),
-    # Note: Wrapping the following values is necessary so that the parameter value can be the empty string
-    "capabilities": ParameterValue(
-      LaunchConfiguration("capabilities"), value_type=str
-    ),
-    "disable_capabilities": ParameterValue(
-      LaunchConfiguration("disable_capabilities"), value_type=str
-    ),
-    # Publish the planning scene of the physical robot so that rviz plugin can know actual robot
     "publish_planning_scene": should_publish,
     "publish_geometry_updates": should_publish,
     "publish_state_updates": should_publish,
